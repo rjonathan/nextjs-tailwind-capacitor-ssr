e2d49a989fdf1603781444480ea36bdf850a739a
Jonathan Rossi <j.rossi@alphalives.com>
2022-07-15 17:50:23 +0200

    add api call with SWR

--- src/pages/page2.js
+++ src/pages/page2.js
@@ -1,29 +1,46 @@
 import Layout from '@/components/layout/appLayout'
 import PageLayout from '@/components/layout/pageLayout'
+import useSWR from 'swr'
 
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 
 function Page() {
+
+    const { data, error } = useSWR('https://random-data-api.com/api/beer/random_beer?size=30', fetcher)
+
+
+    if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+
     return (
         <>
-         <PageLayout>
-      <h1 className="text-3xl font-bold underline">
-        Page 2
-      </h1></PageLayout>
-
-
-       
+            <PageLayout>
+                <h1 className="text-3xl font-bold underline">
+                    Page 2
+                </h1>
+                
+                {data.map(beer => (
+                     <div>
+                        { beer.name }
+                     
+                     </div>
+                ))}
+                
+                </PageLayout>
         </>
     )
 
-  }
-  
-  export default Page
+}
 
-  Page.getLayout = function getLayout(page) {
+export default Page
+
+Page.getLayout = function getLayout(page) {
     return (
-      <Layout  menuCurrent='page2'>
+        <Layout menuCurrent='page2'>
             {page}
-      </Layout>
+        </Layout>
     )
-  }
+}
