e56cbebac8b3aaa20c2d4215c13e3f59ac8f01a1
Jonathan Rossi <j.rossi@alphalives.com>
2022-07-20 13:17:35 +0200

    pwa apple touch icon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -21,6 +21,14 @@ function App({ Component, pageProps: { ...pageProps } }) {
     <meta name='msapplication-tap-highlight' content='no' />
     <meta name='theme-color' content='#000000' />
 
+    <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />
+    <link rel='apple-touch-icon' sizes='152x152' href='/icons/apple-touch-icon-152x152.png' />
+    <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon-180x180.png' />
+    <link rel='apple-touch-icon' sizes='144x144' href='/icons/apple-touch-icon-144x144.png' />
+
+
+
+    <link rel='shortcut icon' href='/favicon.ico' />
 
     <link rel='manifest' href='/manifest.json' />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
